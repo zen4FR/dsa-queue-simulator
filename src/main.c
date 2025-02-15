bbe8cede8fced7b214279deed032735a1ebf4e11
zen4FR <152889284+zen4FR@users.noreply.github.com>
2025-02-15 10:52:16 +0545

    playing around for best bg color

--- src/main.c
+++ src/main.c
@@ -7,7 +7,8 @@ void initializeSDL(SDL_Window **window, SDL_Renderer **renderer) {
     SDL_Init(SDL_INIT_VIDEO);
     *window = SDL_CreateWindow("Traffic Simulation", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
     *renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_SetRenderDrawColor(*renderer, 255, 255, 255, 255); // Set background color to white
+    SDL_SetRenderDrawColor(*renderer, 173, 216, 230, 255); // Light blue
+ 
 }
 
 
