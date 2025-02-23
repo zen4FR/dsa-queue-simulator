2e51a9cb587f1e9b5b256c7f7326468b54639d57
zen4FR <152889284+zen4FR@users.noreply.github.com>
2025-02-23 19:51:50 +0545

    Added frame rate control for smooth animation

--- src/main.c
+++ src/main.c
@@ -3,11 +3,14 @@
 #include <time.h>
 #include "traffic_simulation.h"
 
+#define FPS 60
+#define FRAME_DELAY (1000 / FPS)
+
 void initializeSDL(SDL_Window **window, SDL_Renderer **renderer) {
     SDL_Init(SDL_INIT_VIDEO);
     *window = SDL_CreateWindow("Traffic Simulation", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
     *renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_SetRenderDrawColor(*renderer, 173, 216, 230, 255); // Light blue
+    SDL_SetRenderDrawColor(*renderer, 144, 238, 144, 255); // Light blue
  
 }
 
@@ -20,7 +23,7 @@ void cleanupSDL(SDL_Window *window, SDL_Renderer *renderer) {
 
 void handleEvents(bool *running) {
     SDL_Event event;
-    while (SDL_PollEvent(&event)) {
+    while (SDL_PollEvent(&event)) { 
         if (event.type == SDL_QUIT) {
             *running = false;
         }
