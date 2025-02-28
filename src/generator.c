647d1485a1e4703f89ae96e3aeebc8ad1f6085cc
zen4FR <152889284+zen4FR@users.noreply.github.com>
2025-02-28 21:29:17 +0545

    vehicles spawn time and other changes

--- src/generator.c
+++ src/generator.c
@@ -1,51 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include "traffic_simulation.h"
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <time.h>
+    #include "traffic_simulation.h"
 
-void writeVehicleToFile(FILE *file, Vehicle *vehicle) {
-    fprintf(file, "%f %f %d %d %d %d %d\n", 
-            vehicle->x, vehicle->y, 
-            vehicle->direction, 
-            vehicle->type, 
-            vehicle->turnDirection, 
-            vehicle->state, 
-            vehicle->speed);
-}
-
-int SDL_main(int argc, char *argv[]) {
-    srand(time(NULL));
-    FILE *file = fopen("bin/vehicles.txt", "w");
-    if (!file) {
-        perror("Failed to open vehicles.txt");
-        return 1;
+    void writeVehicleToFile(FILE *file, Vehicle *vehicle) {
+        fprintf(file, "%f %f %d %d %d %d %d\n", 
+                vehicle->x, vehicle->y, 
+                vehicle->direction, 
+                vehicle->type, 
+                vehicle->turnDirection, 
+                vehicle->state, 
+                vehicle->speed);
     }
 
-    printf("Vehicle generation started...\n"); // Debug print statement (1st extra line)
+    int SDL_main(int argc, char *argv[]) {
+        srand(time(NULL));
+        FILE *file = fopen("bin/vehicles.txt", "w");
+        if (!file) {
+            perror("Failed to open vehicles.txt");
+            return 1;
+        }
 
-    while (1) {
-        // Generation of a new vehicle
-        Direction spawnDirection = (Direction)(rand() % 4);
-        Vehicle *newVehicle = createVehicle(spawnDirection);
+        printf("Vehicle generation started...\n"); // Debug print statement (1st extra line)
 
-        printf("Generated vehicle at direction: %d\n", spawnDirection); // Debug print (2nd extra line)
+        while (1) {
+            // Generation of a new vehicle
+            Direction spawnDirection = (Direction)(rand() % 4);
+            Vehicle *newVehicle = createVehicle(spawnDirection);
 
-        // Write the vehicle data to the file
-        writeVehicleToFile(file, newVehicle);
-        fflush(file); // Ensure data is written to the file immediately
+            printf("Generated vehicle at direction: %d\n", spawnDirection); // Debug print (2nd extra line)
 
-        // Free the vehicle memory
-        free(newVehicle);
+            // Write the vehicle data to the file
+            writeVehicleToFile(file, newVehicle);
+            fflush(file); // Ensure data is written to the file immediately
 
-        // Wait for a short period before generating the next vehicle
-        SDL_Delay(1000); // 2 seconds delay
+            // Free the vehicle memory
+            free(newVehicle);
+
+            // Wait for a short period before generating the next vehicle
+            SDL_Delay(2000); // 2 seconds delay
+        }
+
+        //vehicle gen delay added
+        printf("Vehicle generation loop ended.\n"); // Debug print (3rd extra line)
+        
+        fclose(file);
+        printf("File closed successfully.\n"); // Debug print (4th extra line)
+        
+        return 0;
     }
-
-    //vehicle gen delay added
-    printf("Vehicle generation loop ended.\n"); // Debug print (3rd extra line)
-    
-    fclose(file);
-    printf("File closed successfully.\n"); // Debug print (4th extra line)
-    
-    return 0;
-}
